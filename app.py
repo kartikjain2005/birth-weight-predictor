305837425f949574fd960eecd5b8d2192b7db4c6
Kartik jain <kj2855358@gmail.com>
2026-02-20 17:20:39 +0530

    my second commit

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ def get_prediction():
     baby_data_cleaned = get_cleaned_data(baby_data_form)
     baby_df = pd.DataFrame(baby_data_cleaned)
 
-    with open('model/model.pkl', 'rb') as obj:
+    with open('model.pkl', 'rb') as obj:
         model = pickle.load(obj)
 
     prediction = model.predict(baby_df)
